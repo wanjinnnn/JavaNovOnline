3bd9eb8ef7f22d125f00a161f2fe37f824074e92
wanjinnnn <jintan3212@gmail.com>
2024-11-26 17:16:43 +0800

    This is the first commit

---
+++ Person.java
@@ -0,0 +1,16 @@
+import java.text.DecimalFormat;
+
+public class Person {
+
+    public static void main(String[] args) {
+        String name = "Jin";
+        int age = 22;
+        double height = 1.66;
+        double weight = 52;
+        double bmi = weight/height;
+        DecimalFormat df = new DecimalFormat("#.000");
+        System.out.println("The student name is " + name + " and age is " + age);
+        System.out.println("The student height is " + height + " m and the weight is " + weight + " kg");
+        System.out.println("The student bmi is " + df.format(bmi) );
+    }
+}
