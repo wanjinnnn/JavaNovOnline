cbff83bc75085abaaff69e723d495fa4c0170633
wanjinnnn <jintan3212@gmail.com>
2024-11-30 16:47:27 +0800

    Assignment 5 VIP Cheap car

---
+++ assignment5.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class assignment5 {
+    public static void main(String[] args) {
+        //Use scanner to get the prices of the 5 different cars from the terminal and if the value of the car is equal or above 70000 RM, put them in the matrix of 22 which shows the value, "VIP", "2024", "pre-order" and if the value is below the 70000 put them in the matrix of 22 which shows the value, "Cheap", "old", "for sale"
+        String [][] matrixVIP=new String[5][4];
+        String [][] matrixCheap=new String[5][4];
+        double price=0;
+        Scanner scanner=new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            System.out.print("Please input the price of the car "+(i+1)+":");
+            price=scanner.nextInt();
+            if (price>=70000){
+                String price2=Double.toString(price);
+                matrixVIP[i][0]=price2;matrixVIP[i][1]="VIP";matrixVIP[i][2]="2024";matrixVIP[i][3]="pre-order";
+            }
+            else{
+                String price2=Double.toString(price);
+                matrixCheap[i][0]=price2;matrixCheap[i][1]="Cheap";matrixCheap[i][2]="old";matrixCheap[i][3]="for sale";
+            }
+        }
+
+        System.out.print("\nCars in VIP category:");
+        for (int i=0;i<5;i++) {
+            if(matrixVIP[i][0]!=null){
+                System.out.print("\n");
+                for(int j=0;j<4;j++){
+                    System.out.print(matrixVIP[i][j]+" ");
+                }
+            }
+        }
+
+        // Display the Cheap matrix
+        System.out.print("\n\nCars in Cheap category:");
+        for (int i=0;i<5;i++) {
+            if(matrixCheap[i][0]!=null){
+                System.out.print("\n");
+                for(int j=0;j<4;j++){
+                    System.out.print(matrixCheap[i][j]+" ");
+                }
+            }
+        }
+    }
+}
